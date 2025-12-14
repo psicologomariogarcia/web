84b471089731555faf023cc173465a4748dd2d57
Mario Garcia <psicologomariogarcia@gmail.com>
2025-12-14 19:32:49 +0100

    chore: change env

--- db/index.ts
+++ db/index.ts
@@ -7,10 +7,10 @@ import * as schema from "./schema"
 let pool: Pool | undefined
 
 const createPool = () => {
-  const connectionString = process.env.DATABASE_URL
+  const connectionString = process.env.POSTGRES_URL
 
   if (!connectionString) {
-    throw new Error("DATABASE_URL is not set")
+    throw new Error("POSTGRES_URL is not set")
   }
 
   const config: PoolConfig = { connectionString }
